562d9a52c862ba6f992aec268c4f6cb9aeb2d241
Adelar da Silva Queiróz <adelarsq@gmail.com>
2016-09-08 22:00:34 -0300

    Adding screenshot

--- gophervids.go
+++ gophervids.go
@@ -1,59 +1,60 @@
 package gophervids
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"time"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
+    "strings"
+    "time"
 
-	"github.com/gorilla/feeds"
+    "github.com/gorilla/feeds"
 )
 
 type vid struct {
-	Date     string
-	Added    string
-	Id       string
-	Tags     []string
-	Speakers []string
-	Title    string
+    Date     string
+    Added    string
+    Id       string
+    Tags     []string
+    Speakers []string
+    Title    string
 }
 
 func init() {
 
-	js, _ := ioutil.ReadFile("static/vids.json")
+    js, _ := ioutil.ReadFile("static/vids.json")
 
-	var vids []vid
+    var vids []vid
 
-	json.Unmarshal(js, &vids)
+    json.Unmarshal(js, &vids)
 
-	feed := &feeds.Feed{
-		Title:       "gophervids",
-		Link:        &feeds.Link{Href: "http://gophervids.appspot.com"},
-		Description: "categorized golang videos",
-		Author:      &feeds.Author{"Damian Gryski", "damian@gryski.com"},
-	}
+    feed := &feeds.Feed{
+        Title:       "gophervids",
+        Link:        &feeds.Link{Href: "https://rustvids.github.io/"},
+        Description: "categorized rustlang videos",
+        Author:      &feeds.Author{"Adelar da Silva Queiróz", ""},
+    }
 
-	for _, v := range vids {
-		created, _ := time.Parse("2006-01-02", v.Date)
-		updated, _ := time.Parse("2006-01-02", v.Added)
-		speaker := strings.Join(v.Speakers, ", ")
-		feed.Items = append(feed.Items, &feeds.Item{
-			Title:   v.Title,
-			Link:    &feeds.Link{Href: "https://youtu.be/" + v.Id},
-			Author:  &feeds.Author{speaker, ""},
-			Created: created,
-			Updated: updated,
-		})
-	}
+    for _, v := range vids {
+        created, _ := time.Parse("2006-01-02", v.Date)
+        updated, _ := time.Parse("2006-01-02", v.Added)
+        speaker := strings.Join(v.Speakers, ", ")
+        feed.Items = append(feed.Items, &feeds.Item{
+            Title:   v.Title,
+            Link:    &feeds.Link{Href: "https://youtu.be/" + v.Id},
+            Author:  &feeds.Author{speaker, ""},
+            Created: created,
+            Updated: updated,
+        })
+    }
 
-	feed.Created = feed.Items[0].Updated
+    feed.Created = feed.Items[0].Updated
 
-	http.HandleFunc("/rss.xml", func(w http.ResponseWriter, r *http.Request) {
-		feed.WriteRss(w)
-	})
+    http.HandleFunc("/rss.xml", func(w http.ResponseWriter, r *http.Request) {
+        feed.WriteRss(w)
+    })
 
-	http.HandleFunc("/atom.xml", func(w http.ResponseWriter, r *http.Request) {
-		feed.WriteAtom(w)
-	})
+    http.HandleFunc("/atom.xml", func(w http.ResponseWriter, r *http.Request) {
+        feed.WriteAtom(w)
+    })
 }
+
